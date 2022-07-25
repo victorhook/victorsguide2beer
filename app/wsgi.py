61a6bee151805c02adb69ecb7f9d0b781993fca0
victor <victorkrook>
2022-07-25 19:00:30 +0000

    Fixed wsgi run-script

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import sys
-from app.main import app
 
+# SKIP THIS when running apache: if __name__ == '__main__':
 
-if __name__ == '__main__':
-    BASE_PATH = Path(__file__).absolute().parent
-    sys.path.append(str(BASE_PATH))
+BASE_PATH = Path(__file__).absolute().parent
+sys.path.append(str(BASE_PATH))
+sys.path.append(str(BASE_PATH.parent))
 
-    from app import app
+# We just import the application, do not run it, if using apache!
+from app.main import app as application
